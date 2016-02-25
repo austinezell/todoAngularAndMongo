f759bfec6929e516b8e992c9afce04cf67b05fed
austin ezell <austin.r.ezell@gmail.com>
2016-02-24 17:39:31 -0800

    begins to modularize code

---
+++ public/javascripts/controller.js
@@ -0,0 +1,78 @@
+"use strict";
+
+app.controller('mainCtrl', function($scope, $http) {
+  angular.element(document).ready(function() {
+    $(document).foundation();
+  })
+  $scope.task = {};
+  $scope.displayTodos =[];
+  $scope.location = {};
+
+  let d = new Date();
+  let y = d.getFullYear();
+  let m = d.getMonth() < 10 ? `0${d.getMonth()+1}` : d.getMonth() + 1;
+  let day = d.getDate();
+  $scope.today = `${y}-${m}-${day}`;
+
+  function filter(todos) {
+    $scope.displayTodos = todos.filter((todo)=>{
+      return !todo.isCompleted;
+    });
+    $scope.$apply
+  }
+
+  function populate() {
+    $http.get('/todo')
+    .then(function(response) {
+      let todos = response.data.map((todo)=>{
+        todo.datePosted = new Date(todo.datePosted);
+        if(todo.deadline) todo.deadline = new Date(todo.deadline);
+        if(todo.dateCompleted) todo.dateCompleted = new Date(todo.dateCompleted);
+        return todo;
+      })
+      $scope.todos = todos;
+      filter(todos);
+    })
+  }
+
+  $scope.addTask = function(task) {
+    $http.post('/addtodo/', task)
+    .then(function(response) {
+      populate();
+      $scope.task = {};
+    })
+  }
+  $scope.applyFilter = (type) =>{
+
+  }
+  $scope.remove = function(id) {
+    swal({
+      title: "Are you sure?",
+      text: "",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "red",
+      confirmButtonText: "Yes, delete it!",
+      closeOnConfirm: false
+    }, function() {
+      $http.delete('/remove/' + id)
+      .then(function(response) {
+        populate();
+        swal("Deleted!", "Todo removed", "success");
+      }, function(response){
+        console.log(response);
+      })
+    });
+  }
+
+  $scope.toggleComplete = function(id, newState) {
+    $http.put('/update/' + id, {
+      isCompleted: newState
+    })
+    .then(function(data) {
+      populate();
+    })
+  }
+
+  populate();
+})
--- public/javascripts/module.js
+++ public/javascripts/module.js
@@ -4,90 +4,42 @@ var app = angular.module('sample', ['ui.router']);
 
 app.config(function($urlRouterProvider, $stateProvider) {
   $stateProvider
-  .state('home', {
-    url: '/',
-    templateUrl: '/templates/home.html',
+  // .state('home', {
+  //   url: '/',
+  //   template: "<ui-view/>",
+  //   controller: "mainCtrl"
+  // })
+  .state("tasks", {
+    url: "/tasks",
+    templateUrl: '/templates/todos.html',
+    abstract: true,
     controller: "mainCtrl"
   })
-  $urlRouterProvider.otherwise('/')
-})
-
-app.controller('mainCtrl', function($scope, $http) {
-  angular.element(document).ready(function() {
-    $(document).foundation();
+  .state("tasks.todo", {
+    url: "/todo",
+    controller: function($scope, $state) {
+      console.log('loaded');
+      $scope.location.name = $state.current.name.replace('tasks.', '');
+      console.log("hey",$scope.location.name);
+    }
   })
-  $scope.task = {}
+  .state("tasks.done", {
+    url: "/done",
+    controller: function($scope, $state) {
+      console.log('loaded');
+      $scope.location.name = $state.current.name.replace('tasks.', '');
+      console.log("hey",$scope.location.name);
+    }
 
-  let d = new Date();
-  let y = d.getFullYear();
-  let m = d.getMonth() < 10 ? `0${d.getMonth()+1}` : d.getMonth() + 1;
-  let day = d.getDate();
-  $scope.today = `${y}-${m}-${day}`;
-
-  function filter(todos) {
-    $scope.doneTodos= todos.filter((todo)=>{
-      return todo.isCompleted;
-    });
-    $scope.notDoneTodos = todos.filter((todo)=>{
-      return !todo.isCompleted;
-    });
-  }
-
-  function populate() {
-    $http.get('/todo')
-    .then(function(response) {
-      let todos = response.data.map((todo)=>{
-        todo.datePosted = new Date(todo.datePosted);
-        if(todo.deadline) todo.deadline = new Date(todo.deadline);
-        if(todo.dateCompleted) todo.dateCompleted = new Date(todo.dateCompleted);
-        return todo;
-      })
-      $scope.todos = todos;
-      filter(todos);
-    })
-  }
-
-  $scope.addTask = function(task) {
-    $http.post('/addtodo/', task)
-    .then(function(response) {
-      populate();
-      $scope.task = {};
-    })
-  }
-
-
-
-  $scope.remove = function(id) {
-    swal({
-      title: "Are you sure?",
-      text: "",
-      type: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "red",
-      confirmButtonText: "Yes, delete it!",
-      closeOnConfirm: false
-    }, function() {
-      $http.delete('/remove/' + id)
-      .then(function(response) {
-        populate();
-        swal("Deleted!", "Todo removed", "success");
-      }, function(response){
-        console.log(response);
-      })
-    });
-  }
-
-  $scope.toggleComplete = function(id, newState) {
-    console.log('id', id, 'newState', newState);
-    $http.put('/update/' + id, {
-      isCompleted: newState
-    })
-    .then(function(data) {
-      populate();
-    })
-  }
-
-  populate();
+  })
+  .state("tasks.all", {
+    url: "/all",
+    controller: function($scope, $state) {
+      console.log('loaded');
+      $scope.location.name = $state.current.name.replace('tasks.', '');
+      console.log("hey",$scope.location.name);    }
+  })
+  $urlRouterProvider.otherwise('/tasks/todo')
 })
 
 app.directive("foundationRepeat", function() {
